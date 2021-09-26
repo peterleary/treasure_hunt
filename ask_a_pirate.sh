e16373c1b655fceca6d6fa12f88ac1825ce4ce31
Robin Engler <robin.engler@sib.swiss>
2021-09-26 22:23:33 +0200

    Castle main gate

---
+++ ask_a_pirate.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+source .script/quest_functions.sh
+
+# Quest starts here...
+pirate_say "Yes, that's the famous Blackbeard castle, it was one the home of the legendary pirate.\n"
+sleep 3
+echo -e "For a silver coin I can show you around!\n"
+sleep 3
+
+echo "What?? You want to use Blackbeard's flag! Are you serious?"
+sleep 3
+
+echo "OK, well, if you're sure... let me see..."
+sleep 3
+
+echo "Check-out the old watch tower, but tread carefully..."
+sleep 2
+echo "And don't say I didn't warn you!"
+sleep 2
+echo "Now, how about that castle tour?"
+
+exit 0
